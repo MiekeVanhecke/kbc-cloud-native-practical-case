63fc986e3bd8c59b7fbb4a38940685ab3b987f24
u32331 <mieke.vanhecke@kbc.be>
2022-12-01 09:57:37 +0100

    Lab 06

--- ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingController.java
+++ ezGroceriesShoppingList/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingController.java
@@ -9,9 +9,14 @@ import com.ezgroceries.shoppinglist.dao.repositories.CocktailDBClient;
 import com.ezgroceries.shoppinglist.dao.ShoppingListResource;
 import com.ezgroceries.shoppinglist.services.CocktailService;
 import com.ezgroceries.shoppinglist.services.ShoppingListService;
+import org.apache.catalina.connector.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -37,6 +42,16 @@ public class ShoppingController {
         this.shoppingListService = shoppingListService;
     }
 
+    //Custom tomcat configuration, we add a connector that allows http traffic next to https
+    @Bean
+    public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort){
+        Connector connector=new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
     @GetMapping(path="/cocktails", produces = "application/json")
     public List<CocktailResource> cocktails(@RequestParam(value = "search", required = false) String search) {
         CocktailDBResponse cocktailDBResponse = cocktailDBClient.searchCocktails(search);
